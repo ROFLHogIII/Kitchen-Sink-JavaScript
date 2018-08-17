2498f6bf48e3cb0efb2ff1223ff597e75950b9ef
Richard Garner <richardgarner@mac.com>
2018-08-17 16:18:12 -0500

    4:15 save point

--- app.js
+++ app.js
@@ -14,6 +14,29 @@ if (name.charCodeAt(index) == L.charCodeAt(index)) {
 //as long as the Character code of the name is equal to the character code of the letter in Variable "L", "Next" will be displayed. Elsewise, "Back of the Line" will be displayed.
 
 function sayHello() {
-    console.log('Hello world')
+    console.log('Hello world');
 }
 sayHello();
+
+//wow, this one was a *** to work on.
+var names = ['Charles', 'Abby', 'James', 'John'];
+var ages = [21, 27, 18, 17];
+
+
+for (let index = 0; index < 4; index++) {
+    checkAge(names[index], ages[index]);
+}
+
+function checkAge(name, age) {
+    if (age < 21) {
+        console.log("Sorry " + name + ", you aren't old enough to view this page!")
+    } else {
+        console.log('Congrats ' + name + ', you did the thing!')
+    }
+};
+
+var veggies = ['Cucuber', 'Broccoli', 'Spinach', 'Squash']
+for (let index = 0; index < veggies.length; index++) {
+    console.log(veggies[index])
+    
+}
