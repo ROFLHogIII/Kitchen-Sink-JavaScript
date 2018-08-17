b293fcf7e87934af01149034595593b784da8318
Richard Garner <richardgarner@mac.com>
2018-08-17 13:54:58 -0500

    2 P.M. save point

---
+++ app.js
@@ -0,0 +1,14 @@
+var name = 'Richard Garner';
+const states = 50;
+var num1 = 5 + 4;
+console.log(name, states, num1)
+
+var index = 0; //set an index variable to be 0
+var L = 'L'//created a desired variable with only one letter, the desireable one
+
+if (name.charCodeAt(index) == L.charCodeAt(index)) {
+    console.log('Next!')
+} else {
+    console.log('Back of the line!')
+}
+//as long as the Character code of the name is equal to the character code of the letter in Variable "L", "Next" will be displayed. Elsewise, "Back of the Line" will be displayed.
